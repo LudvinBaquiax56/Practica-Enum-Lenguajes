1c795a1437f6743112351b026d2f97e914eabdbd
LudvinBaquiax56 <vasqueznicolas56@gmail.com>
2021-08-05 21:49:02 -0600

    Creacion de GUI completada

--- PracticaEnum/src/practicaenum/PracticaEnum.java
+++ PracticaEnum/src/practicaenum/PracticaEnum.java
@@ -5,6 +5,8 @@
  */
 package practicaenum;
 
+import GUI.Ventana;
+
 /**
  *
  * @author baquiax
@@ -16,6 +18,7 @@ public class PracticaEnum {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Ventana ventana = new Ventana();
     }
     
 }
